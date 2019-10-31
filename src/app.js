3863f589010d1b2d64884b3a79c5f60836897704
Keisuke Isobe <keisuke.isobe.2011@gmail.com>
2019-10-31 15:12:03 -0400

    first commit

--- src/app.js
+++ src/app.js
@@ -3,20 +3,34 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const logger = require('./logger');
+const bookmarksRouter = require('./bookmarks-router');
 
 const app = express();
 
-const morganOption = (process.env.NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
-
+const morganOption = (process.env.NODE_ENV === 'production') ? 'tiny' : 'common';
+console.log(process.env);
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.use(express.json());
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  next();
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.send('made it to home');
 });
+
+app.use(bookmarksRouter);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
